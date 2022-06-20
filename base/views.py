a08bd432c87be6fe133f5ca93683d436afba29e4
Mohammed Adib <76277079+ADIB-the-NOOB@users.noreply.github.com>
2022-06-20 19:09:18 +0600

    first commit

---
+++ base/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def  home(request):
+    return render(request, 'base/home.html')
+
+def room(request,pk):
+    rooms = [{'id':1,'name': 'Learn Pthon'},
+                        {'id':2,'name': 'Learn Java'},
+                        {'id':3,'name': 'Learn C++'},]
+    
+    return render(request, 'base/room.html',{'rooms':rooms})
