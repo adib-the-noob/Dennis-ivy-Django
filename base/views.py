ed48cd11139bd2ef32489773e4bdd875069ec3be
ADIB-the-NOOB <mdadib550@gmail.com>
2022-06-21 19:55:32 +0600

    Rendering Data from Database

--- base/views.py
+++ base/views.py
@@ -1,20 +1,22 @@
 from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Room
 
-rooms = [{'id':1,'name': 'Learn Python'},
-                        {'id':2,'name': 'Learn Java'},
-                        {'id':3,'name': 'Learn C++'},]
+rooms = [{'id': 1, 'name': 'Learn Python'},
+         {'id': 2, 'name': 'Learn Java'},
+         {'id': 3, 'name': 'Learn C++'}, ]
 
 # Create your views here.
-def  home(request):
-    context={'rooms':rooms}
-    return render(request, 'base/home.html',context)
 
-def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-    context = {'room':room}
-    return render(request, 'base/room.html',context)
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
