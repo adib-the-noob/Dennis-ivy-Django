4257fd92ad3b2f2dcb83a8c502145964c8bd16c4
Mohammed Adib <76277079+ADIB-the-NOOB@users.noreply.github.com>
2022-06-28 18:09:29 +0600

    User Login and Flash messge

--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 from django.db.models import Q
 from base.forms import RoomForm
 from .models import Room,Topic
@@ -9,6 +11,19 @@ from .models import Room,Topic
 #       {'id': 3, 'name': 'Learn C++'}, ]
 
 # Create your views here.
+
+def loginPage(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User Does Not Exist!')
+        
+    context = {}
+    return render(request,'base/login_register.html',context)
 
 
 def home(request):
